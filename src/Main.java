05a601f8d7b3f881c29ac34aea8a6220c72871d4
matteogeertsema <m.geertsema@st.hanze.nl>
2019-04-01 11:08:23 +0200

    added column and row numbers in buttons BoardView

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
         BoardView boardView = new BoardView();
         boardView.paint(boardView);
     }
--- src/view/BoardView.java
+++ src/view/BoardView.java
@@ -2,6 +2,7 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class BoardView extends JPanel {
 
@@ -20,6 +21,9 @@ public class BoardView extends JPanel {
                 b.setBorder(BorderFactory.createLineBorder(Color.black));
                 b.setBackground(Color.GREEN);
                 button[i][j] = b;
+                button[i][j].putClientProperty("column", i);
+                button[i][j].putClientProperty("row", j);
+                button[i][j].addActionListener(new MyActionListener());
                 tiles.add(button[i][j]);
             }
         }
@@ -34,5 +38,14 @@ public class BoardView extends JPanel {
         frame.setVisible(true);
     }
 
+    public class MyActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton btn = (JButton) e.getSource();
+            System.out.println("column " + btn.getClientProperty("column")
+                    + ", row " + btn.getClientProperty("row"));
+        }
+    }
 }
 
