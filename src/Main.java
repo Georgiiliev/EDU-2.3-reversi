a1e31b18233f4361a73677b2759284f4155aa24a
wouter <w.greven@st.hanze.nl>
2019-04-01 13:33:57 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        new MovementController("localhost");
+        //new MovementController("localhost");
         launch(args);
     }
 }
