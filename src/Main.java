d439c2e98015c05fca28ebc588cda24332d0bca7
wouter <w.greven@st.hanze.nl>
2019-04-01 14:05:57 +0200

    ok2

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         //new MovementController("localhost");
-=======
->>>>>>> parent of 0f071d3... Update
         launch(args);
 
     }
